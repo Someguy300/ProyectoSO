c446ea7573c900710db18e92d1ba5841d2b44014
Someguy300 <Someguy300@users.noreply.github.com>
2020-06-04 14:37:15 -0400

    inicio

--- src/proyectoso/Cliente.java
+++ src/proyectoso/Cliente.java
@@ -9,14 +9,6 @@ package proyectoso;
  *
  * @author Jesus Barrios
  */
-public class ProyectoSO {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello");
-    }
+public class Cliente {
     
 }
